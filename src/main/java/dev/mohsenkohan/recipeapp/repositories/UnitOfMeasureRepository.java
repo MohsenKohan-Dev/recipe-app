f876f620c3352e3d9866744a145fd2b28fbab852
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-03-28 19:54:50 +0430

    Adding Spring Data JPA Query Methods

--- src/main/java/dev/mohsenkohan/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/dev/mohsenkohan/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package dev.mohsenkohan.recipeapp.repositories;
 import dev.mohsenkohan.recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
