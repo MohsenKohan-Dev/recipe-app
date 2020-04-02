212694037fbcb28c1e9a412228e1920decf4708a
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-04-02 18:05:28 +0430

    Improving IndexControllerTest with ArgumentCaptor

--- src/test/java/dev/mohsenkohan/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/dev/mohsenkohan/recipeapp/controllers/IndexControllerTest.java
@@ -1,17 +1,21 @@
 package dev.mohsenkohan.recipeapp.controllers;
 
+import dev.mohsenkohan.recipeapp.domain.Recipe;
 import dev.mohsenkohan.recipeapp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -23,6 +27,9 @@ class IndexControllerTest {
 
     IndexController indexController;
 
+    @Captor
+    ArgumentCaptor<Set<Recipe>> captor;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -31,11 +38,19 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+        when(recipeService.getRecipes()).thenReturn(Set.of(new Recipe()));
+
+//        ArgumentCaptor<Set<Recipe>> captor = ArgumentCaptor.forClass(Set.class);
+
         String indexPage = indexController.getIndexPage(model);
 
         assertEquals("index", indexPage);
 
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), captor.capture());
+
+        Set<Recipe> recipes = captor.getValue();
+
+        assertEquals(1, recipes.size());
     }
 }
